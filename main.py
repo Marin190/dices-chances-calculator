2355c83285433aa7175be3a14d7bec744728da70
Marin <74853069+Marin190@users.noreply.github.com>
2022-06-04 10:01:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import random, time, sys, threading
+import matplotlib.pyplot as plt
+
+try: launch = int(input("How many roll > "))
+except : print("invalid number"), sys.exit()
+try: dices = int(input("How many dices > "))
+except : print("invalid number"), sys.exit()
+
+a = (dices * 6) - (dices - 1)
+Fresult = {}
+for i in range(a): Fresult[str(i + dices)] = 0
+
+def main():
+	global Fresult
+	result = 0
+	for i in range(dices):
+		result = result + random.randint(1, 6)
+	Fresult[str(result)] = Fresult[str(result)] + 1
+
+threading.Thread(target=main)
+for i in range(launch): threading.Thread(target=main).start()
+
+left = []
+for i in range(a): left.append(i + dices)
+height = []
+for i in range(a): height.append(Fresult[str(i + dices)])
+tick_label = []
+for i in range(a): tick_label.append(str(i + dices))
+
+plt.bar(left, height, tick_label = tick_label, width = 0.5, color = ['red', 'green'])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Dices algo')
+plt.show()
