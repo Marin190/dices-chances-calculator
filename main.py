cde08d3c7f1b236c4b48f703ae2b2a55fad58820
Marin <74853069+Marin190@users.noreply.github.com>
2022-06-11 12:10:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import random, sys, threading
 import matplotlib.pyplot as plt
-
 try: launch = int(input("How many roll > "))
 except : print("invalid number"), sys.exit()
 try: dices = int(input("How many dices > "))
 except : print("invalid number"), sys.exit()
-
 a = (dices * 6) - (dices - 1)
 Fresult = {}
 for i in range(a): Fresult[str(i + dices)] = 0
-
 def main():
 	global Fresult
 	result = 0
@@ -19,14 +16,10 @@ def main():
 
 threading.Thread(target=main)
 for i in range(launch): threading.Thread(target=main).start()
-
-left = []
+left = height = tick_label = []
 for i in range(a): left.append(i + dices)
-height = []
 for i in range(a): height.append(Fresult[str(i + dices)])
-tick_label = []
 for i in range(a): tick_label.append(str(i + dices))
-
 plt.bar(left, height, tick_label = tick_label, width = 0.5, color = ['red', 'green'])
 plt.xlabel('x')
 plt.ylabel('y')
